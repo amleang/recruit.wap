384abd7c07337305b21f3072a9200ef6252b497a
少游[赵安良] <shaoyou@jzez100.com>
2018-10-12 17:30:16 +0800

    update

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="content">
     <div class="header-img">
-      <div class="top"></div>
       <div class="avatar">
-fdsafsdafsafaf
+        <div class="user-img">
+          <img src="https://cos.niuzhigongzuo.com/avatar/785e7cd5-62dd-4a07-a727-40c53af6da70-1538326932685.png" alt="">
+        </div>
+        <div class="man-box">
+          <div>安良</div>
+          <div>15050474495</div>
+        </div>
+        <div class="my_qrCode">
+          <i class="iconfont icon-erweima" style="font-size: 0.6rem;"></i>
+        </div>
+      </div>
+    </div>
+    <div class="list-user">
+      <div class="item-content">
+
       </div>
     </div>
   </div>
@@ -28,18 +41,44 @@ export default {
   left: 0;
   overflow: auto;
   -webkit-overflow-scrolling: touch;
+  background-color:#efeff4;
 }
 .header-img {
   background-image: url("../../assets/images/user02.png");
   background-size: 100% 100%;
   overflow: hidden;
 }
-.header-img .top {
-  line-height: 0.5rem;
-  height: 0.5rem;
-}
+
 .header-img .avatar {
   margin: 0.5rem 0 0.5rem 0.5rem;
   display: flex;
+}
+.avatar .user-img {
+  width: 1.6rem;
+  height: 1.6rem;
+  border-radius: 5px;
+  overflow: hidden;
+}
+.avatar .user-img img {
+  display: block;
+  width: 100%;
+}
+.avatar .man-box {
+  padding-left: 0.25rem;
+  font-size: 0.4rem;
+  width: 100%;
+  flex: 1;
+}
+.avatar .man-box div {
+  height: 0.8rem;
+  line-height: 0.8rem;
+}
+.avatar .my_qrCode {
+  margin: 0 0.8rem 0 auto;
+  align-self: center;
+  color: #000;
+}
+.list-user {
+  margin-bottom: 0.25rem;
 }
 </style>
