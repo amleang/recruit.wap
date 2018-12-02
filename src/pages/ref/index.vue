43ebde01deb23964c1af418a0327d3c7a8058c2a
少游 <shaoyou@jzez100.com>
2018-12-02 22:53:37 +0800

    update

--- src/pages/ref/index.vue
+++ src/pages/ref/index.vue
@@ -3,39 +3,59 @@
 </template>
 
 <script>
-import {  setWxItem } from "@/components/lib/util";
+import axios from "axios";
+import { setWxItem } from "@/components/lib/util";
 export default {
   mounted() {
-     const code= this.$route.query.code;
-     alert("code=>"+code);
-     const wx={
-         appid:"wx1124be6bc1512298",
-         secret:"091885925a2232c6b7bf89f2eed30972"
-     }
-     this.http.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid="+wx.appid+"&secret="+wx.secret+"&code="+code+"&grant_type=authorization_code").then(res=>{
-         console.log(res);
-         alert(JSON.stringify(res));
-         if(res.errcode){
-              this.mui.toast("获取用户授权失败！", { duration: "long", type: "div" });
-         }
-         else{
-             this.http.get("https://api.weixin.qq.com/sns/userinfo?access_token="+res.access_token+"&openid="+res.openid+"&lang=zh_CN").then(tres=>{
-                 this.http.post("/api/app/savewxuser",tres).then(sres=>{
-                     if(sres.status==0){
-                         //跳转登录页
-                         this.$router.push({path:"/login?status=con"});
-                     }
-                     else{
-                        this.setWxItem(sres);
-                        this.$router.push({path:"/"});
-                     }
-                 })
-             })
-         }
-     })
+    const code = this.$route.query.code;
+    alert("code=>" + code);
+    const wx = {
+      appid: "wx1124be6bc1512298",
+      secret: "091885925a2232c6b7bf89f2eed30972"
+    };
+    axios
+      .get(
+        "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" +
+          wx.appid +
+          "&secret=" +
+          wx.secret +
+          "&code=" +
+          code +
+          "&grant_type=authorization_code"
+      )
+      .then(res => {
+        console.log(res);
+        alert(JSON.stringify(res));
+        if (res.errcode) {
+          this.mui.toast("获取用户授权失败！", {
+            duration: "long",
+            type: "div"
+          });
+        } else {
+          axios
+            .get(
+              "https://api.weixin.qq.com/sns/userinfo?access_token=" +
+                res.access_token +
+                "&openid=" +
+                res.openid +
+                "&lang=zh_CN"
+            )
+            .then(tres => {
+              this.http.post("/api/app/savewxuser", tres).then(sres => {
+                if (sres.status == 0) {
+                  //跳转登录页
+                  this.$router.push({ path: "/login?status=con" });
+                } else {
+                  this.setWxItem(sres);
+                  this.$router.push({ path: "/" });
+                }
+              });
+            });
+        }
+      });
   },
-  methods:{
-      setWxItem
+  methods: {
+    setWxItem
   }
 };
 </script>
