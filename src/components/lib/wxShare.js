86805f82fa2edc4d692e07d32144b52ab9a3f2dc
少游 <shaoyou@jzez100.com>
2018-12-10 16:38:22 +0800

    update

--- src/components/lib/wxShare.js
+++ src/components/lib/wxShare.js
@@ -57,26 +57,27 @@ export const wxShareConfig = function (title) {
 }
 
 export const wxhideMenu = function () {
-    this.http.post("/api/app/getwxshare", { url: location.href }).then(res => {
-        wx.config({
-            debug: false,
-            appId: res.data.appId,
-            timestamp: res.data.timestamp,
-            nonceStr: res.data.nonceStr,
-            signature: res.data.signature,
-            jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage"]
-        });
-    });
+    // this.http.post("/api/app/getwxshare", { url: location.href }).then(res => {
+    //     wx.config({
+    //         debug: false,
+    //         appId: res.data.appId,
+    //         timestamp: res.data.timestamp,
+    //         nonceStr: res.data.nonceStr,
+    //         signature: res.data.signature,
+    //         jsApiList: ['hideMenuItems']
+    //     });
+    // });
     wx.ready(function () {
         // 判断当前版本是否支持指定 JS 接口，支持批量判断
-        wx.checkJsApi({
-            jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage"],
+        /* wx.checkJsApi({
+            jsApiList: ["hideMenuItems"],
             success: function (res) {
                 console.log(JSON.stringify(res));
             }
         });
         wx.hideMenuItems({
             menuList: ["menuItem:share:timeline", "menuItem:copyUrl", "menuItem:share:appMessage", "menuItem:share:qq", "menuItem:share:weiboApp", "menuItem:favorite", "menuItem:share:facebook", "menuItem:share:QZone", "menuItem:editTag", "menuItem:delete", "menuItem:copyUrl", "menuItem:originPage", "menuItem:readMode", "menuItem:openWithQQBrowser", "menuItem:openWithSafari", "menuItem:share:email", "menuItem:share:brand"] // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮
-        });
+        }); */
+        wx.hideOptionMenu();
     });
 }
